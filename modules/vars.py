2b7f43f8b81913dd72a1c04fbf6fcaae0b565e0b
buzihelp-source <buzihelp@gmail.com>
2026-03-31 13:41:35 +0530

    Update API credentials and owner information

--- modules/vars.py
+++ modules/vars.py
@@ -3,15 +3,15 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22282574"))
-API_HASH = environ.get("API_HASH", "06ab0aa02164cfb2d71c3e7223afab1e")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8384555145:AAF1zy3z_QkRCLQv7RuP5pxTb8thcsX3Nhc")
+API_ID = int(environ.get("API_ID", "36123528"))
+API_HASH = environ.get("API_HASH", "7f77eb79febe2b7cf5d33d6d57bc8ac0")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8640143346:AAGUFxqf9PvQa6w9PKalQQYHz_aQzycWAZc")
 
-OWNER = int(environ.get("OWNER", "1278128855"))
+OWNER = int(environ.get("OWNER", "8532700793"))
 CREDIT = environ.get("CREDIT", "𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8222519141').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7831923713').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
